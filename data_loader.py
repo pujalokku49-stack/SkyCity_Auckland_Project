b3ccbdf46510a3cebeff01e74ed8d414e17b76d0
Lokku Puja <pujalokku49@gmail.com>
2026-03-14 14:09:35 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,6 @@ import streamlit as st
 @st.cache_data
 
 def load_data():
-    df = pd.read_csv("data/SkyCity Auckland Restaurants & Bars.csv")
+    df = pd.read_csv("data/skycity_data.csv")
 
     return df
